8ec03e1b4904d4ceb5cb62f7167fc3a9d0d2efd4
AethereusDipanshuDiwaker <123458922+AethereusDipanshuDiwaker@users.noreply.github.com>
2023-08-03 20:52:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,171 @@
+function processUserInput() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json"); // Change content type to JSON
+    myHeaders.append("Cookie", "JSESSIONID=076758C2042073027A9286E20A6A9055");
+    
+    var raw = JSON.stringify({
+        "login_id": "test@sunbasedata.com",
+        "password": "Test@123"
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      credentials: 'include', // Include cookies
+      redirect: 'follow'
+    };
+    
+    fetch("https://qa2.sunbasedata.com/sunbase/portal/api/assignment_auth.jsp", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+}
+
+
+
+
+
+
+function Acess(){
+const xhttp = new XMLHttpRequest();
+xhttp.open("POST", "https://qa2.sunbasedata.com/sunbase/portal/api/assignment_auth.jsp");
+xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+var acess_Token = xhttp.send(JSON.stringify({  
+  "avatar": "https://www.mecallapi.com/users/cat.png",
+  "login_id" : "test@sunbasedata.com",
+  "password" :"Test@123"   
+}));
+console.log(acess_Token.access_token);
+}
+
+function loadTable() {
+    
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://www.mecallapi.com/api/users");
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+        var trHTML = ''; 
+        const objects = JSON.parse(this.responseText);
+        for (let object of objects) {
+          trHTML += '<tr>'; 
+          trHTML += '<td>'+object['id']+'</td>';
+          trHTML += '<td><img width="50px" src="'+object['avatar']+'" class="avatar"></td>';
+          trHTML += '<td>'+object['fname']+'</td>';
+          trHTML += '<td>'+object['lname']+'</td>';
+          trHTML += '<td>'+object['username']+'</td>';
+          trHTML += '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox('+object['id']+')">Edit</button>';
+          trHTML += '<button type="button" class="btn btn-outline-danger" onclick="userDelete('+object['id']+')">Del</button></td>';
+          trHTML += "</tr>";
+        }
+        document.getElementById("mytable").innerHTML = trHTML;
+      }
+    };
+  }
+  function showUserCreateBox() {
+    Swal.fire({
+      title: 'Create user',
+      html:
+        '<input id="id" type="hidden">' +
+        '<input id="fname" class="swal2-input" placeholder="First">' +
+        '<input id="lname" class="swal2-input" placeholder="Last">' +
+        '<input id="username" class="swal2-input" placeholder="Username">' +
+        '<input id="email" class="swal2-input" placeholder="Email">',
+      focusConfirm: false,
+      preConfirm: () => {
+        userCreate();
+      }
+    })
+  }
+  
+  function userCreate() {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+      
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://www.mecallapi.com/api/users/create");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({ 
+      "fname": fname, "lname": lname, "username": username, "email": email, 
+      "avatar": "https://www.mecallapi.com/users/cat.png"
+    }));
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const objects = JSON.parse(this.responseText);
+        Swal.fire(objects['message']);
+        loadTable();
+      }
+    };
+  }
+
+  function showUserEditBox(id) {
+    console.log(id);
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://www.mecallapi.com/api/users/"+id);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const objects = JSON.parse(this.responseText);
+        const user = objects['user'];
+        console.log(user);
+        Swal.fire({
+          title: 'Edit User',
+          html:
+            '<input id="id" type="hidden" value='+user['id']+'>' +
+            '<input id="fname" class="swal2-input" placeholder="First" value="'+user['fname']+'">' +
+            '<input id="lname" class="swal2-input" placeholder="Last" value="'+user['lname']+'">' +
+            '<input id="username" class="swal2-input" placeholder="Username" value="'+user['username']+'">' +
+            '<input id="email" class="swal2-input" placeholder="Email" value="'+user['email']+'">',
+          focusConfirm: false,
+          preConfirm: () => {
+            userEdit();
+          }
+        })
+      }
+    };
+  }
+  
+  function userEdit() {
+    const id = document.getElementById("id").value;
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+      
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("PUT", "https://www.mecallapi.com/api/users/update");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({ 
+      "id": id, "fname": fname, "lname": lname, "username": username, "email": email, 
+      "avatar": "https://www.mecallapi.com/users/cat.png"
+    }));
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const objects = JSON.parse(this.responseText);
+        Swal.fire(objects['message']);
+        loadTable();
+      }
+    };
+  }
+
+  function userDelete(id) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "https://www.mecallapi.com/api/users/delete");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({ 
+      "id": id
+    }));
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        const objects = JSON.parse(this.responseText);
+        Swal.fire(objects['message']);
+        loadTable();
+      } 
+    };
+  }
+
+  processUserInput();
